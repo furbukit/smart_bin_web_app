20065f7a5984b31b06aabdd52df8924723c5905f
furbukit <matt-j-walsh@outlook.com>
2022-10-03 10:29:52 +0800

    Updating UI for webiste

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Container, Grid } from "@mui/material";
+import { AreaSaved } from "./components/dashboard-utils/Area_saved";
+import { WasteTable } from "./components/dashboard-utils/table";
+import { ItemsList } from "./components/dashboard-utils/items_list";
+import { WasteBar } from "./components/dashboard-utils/bar";
+import { TreesPlanted } from "./components/dashboard-utils/trees_planted";
+import { CO2Saved } from "./components/dashboard-utils/CO2_saved";
+import { PieChart } from "./components/dashboard-utils/pie";
+import { RecyclingRate } from "./components/dashboard-utils/recycling_rate";
+import { registerChartJs } from "./components/dashboard-utils/register-chart-js";
+import { ThemeProvider } from "@mui/material/styles";
+import { DashboardNavbar } from "./components/dashboard-navbar";
+import { theme } from "./theme";
+
+registerChartJs();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <DashboardNavbar></DashboardNavbar>
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <RecyclingRate />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <CO2Saved />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <AreaSaved />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TreesPlanted sx={{ height: "100%" }} />
+            </Grid>
+            <Grid item lg={8} md={12} xl={9} xs={12}>
+              <WasteBar />
+            </Grid>
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <PieChart sx={{ height: "100%" }} />
+            </Grid>
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <ItemsList sx={{ height: "100%" }} />
+            </Grid>
+            <Grid item lg={8} md={12} xl={9} xs={12}>
+              <WasteTable />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
-
 export default App;
---
+++ app/src/components/dashboard-navbar.js
@@ -0,0 +1,95 @@
+import { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Grid,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { Bell as BellIcon } from "../icons/bell";
+import { UserCircle as UserCircleIcon } from "../icons/user-circle";
+import { Users as UsersIcon } from "../icons/users";
+import Image from "material-ui-image";
+import logo from "./smartbinlogo.png";
+
+const DashboardNavbarRoot = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[3],
+}));
+
+export const DashboardNavbar = (props) => {
+  const { onSidebarOpen, ...other } = props;
+  const settingsRef = useRef(null);
+  const [openAccountPopover, setOpenAccountPopover] = useState(false);
+
+  return (
+    <>
+      <DashboardNavbarRoot
+        sx={{
+          left: {
+            lg: 0,
+          },
+          width: {
+            lg: "calc(100%)",
+          },
+        }}
+        {...other}
+      >
+        <Toolbar
+          disableGutters
+          sx={{
+            minHeight: 64,
+            left: 0,
+            px: 2,
+          }}
+        >
+          <Box>
+            <img src={logo} width="200px" height="80px" text-align="left" />
+          </Box>
+          <Tooltip title="Search">
+            <IconButton sx={{ ml: 1 }}>
+              <SearchIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Box sx={{ flexGrow: 1 }} />
+          <Tooltip title="Contacts">
+            <IconButton sx={{ ml: 1 }}>
+              <UsersIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Notifications">
+            <IconButton sx={{ ml: 1 }}>
+              <Badge badgeContent={4} color="primary" variant="dot">
+                <BellIcon fontSize="small" />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+          <Avatar
+            onClick={() => setOpenAccountPopover(true)}
+            ref={settingsRef}
+            sx={{
+              cursor: "pointer",
+              height: 40,
+              width: 40,
+              ml: 1,
+            }}
+            src="/static/images/avatars/avatar_1.png"
+          >
+            <UserCircleIcon fontSize="small" />
+          </Avatar>
+        </Toolbar>
+      </DashboardNavbarRoot>
+    </>
+  );
+};
+
+DashboardNavbar.propTypes = {
+  onSidebarOpen: PropTypes.func,
+};
